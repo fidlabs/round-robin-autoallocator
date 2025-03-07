b850dbb05d2edab6f7cc3f5580eaef14a0670cbb
Michał Mach <vipmachu@gmail.com>
2025-03-07 13:51:48 +0100

    filecoin hook

--- src/RoundRobinAllocator.sol
+++ src/RoundRobinAllocator.sol
@@ -5,6 +5,11 @@ import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/U
 import {Ownable2StepUpgradeable} from "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
 import {PausableUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
 
+import {CommonTypes} from "filecoin-solidity/types/CommonTypes.sol";
+import {DataCapTypes} from "filecoin-solidity/types/DataCapTypes.sol";
+import {CBORDecoder} from "filecoin-solidity/utils/CborDecode.sol";
+import {UtilsHandlers} from "filecoin-solidity/utils/UtilsHandlers.sol";
+
 import {Errors} from "./lib/Errors.sol";
 
 contract RoundRobinAllocator is
@@ -12,6 +17,10 @@ contract RoundRobinAllocator is
     Ownable2StepUpgradeable,
     PausableUpgradeable
 {
+    uint32 constant _FRC46_TOKEN_TYPE = 2233613279;
+    address private constant _DATACAP_ADDRESS =
+        address(0xfF00000000000000000000000000000000000007);
+
     function _authorizeUpgrade(
         address newImplementation
     ) internal override onlyOwner {}
@@ -25,5 +34,40 @@ contract RoundRobinAllocator is
 
     function renounceOwnership() public view override onlyOwner {
         revert Errors.OwnershipCannotBeRenounced();
+    }
+
+    /**
+     * @notice The handle_filecoin_method function is a universal entry point for calls
+     * coming from built-in Filecoin actors. Datacap is an FRC-46 Token. Receiving FRC46
+     * tokens requires implementing a Receiver Hook:
+     * https://github.com/filecoin-project/FIPs/blob/master/FRCs/frc-0046.md#receiver-hook.
+     * We use handle_filecoin_method to handle the receiver hook and make sure that the token
+     * sent to our contract is freshly minted Datacap and reject all other calls and transfers.
+     * @param method Method number
+     * @param inputCodec Codec of the payload
+     * @param params Params of the call
+     * @dev Reverts if caller is not a datacap contract
+     * @dev Reverts if trying to send a unsupported token type
+     * @dev Reverts if trying to receive invalid token
+     * @dev Reverts if trying to send a unsupported token
+     */
+    // solhint-disable func-name-mixedcase
+    function handle_filecoin_method(uint64 method, uint64 inputCodec, bytes calldata params)
+        external
+        view
+        returns (uint32 exitCode, uint64 codec, bytes memory data)
+    {
+        if (msg.sender != _DATACAP_ADDRESS) revert Errors.InvalidCaller(msg.sender, _DATACAP_ADDRESS);
+        CommonTypes.UniversalReceiverParams memory receiverParams =
+            UtilsHandlers.handleFilecoinMethod(method, inputCodec, params);
+        if (receiverParams.type_ != _FRC46_TOKEN_TYPE) revert Errors.UnsupportedType();
+        (uint256 tokenReceivedLength, uint256 byteIdx) = CBORDecoder.readFixedArray(receiverParams.payload, 0);
+        if (tokenReceivedLength != 6) revert Errors.InvalidTokenReceived();
+        uint64 from;
+        (from, byteIdx) = CBORDecoder.readUInt64(receiverParams.payload, byteIdx); // payload == FRC46TokenReceived
+        if (from != CommonTypes.FilActorId.unwrap(DataCapTypes.ActorID)) revert Errors.UnsupportedToken();
+        exitCode = 0;
+        codec = 0;
+        data = "";
     }
 }
--- src/lib/Errors.sol
+++ src/lib/Errors.sol
@@ -3,5 +3,23 @@ pragma solidity =0.8.25;
 
 library Errors {
     // @dev Error thrown when renounceOwnership is called
+    // 0x2fab92ca
     error OwnershipCannotBeRenounced();
+
+    /// @dev Thrown if trying to receive ussuported token type
+    // 0xc6de466a
+    error UnsupportedType();
+
+    /// @dev Thrown if trying to receive invalid token
+    // 0x6d5f86d5
+    error InvalidTokenReceived();
+
+    /// @dev Thrown if trying to receive unsupported token
+    // 0x6a172882
+    error UnsupportedToken();
+
+
+    /// @dev Thrown if caller is invalid
+    // 0x6a172882
+    error InvalidCaller(address caller, address expectedCaller);
 }
