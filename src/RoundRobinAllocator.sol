696404bcca3ea21f902986c91b8e9f11cad46474
Michał Mach <vipmachu@gmail.com>
2025-03-07 12:24:48 +0100

    round robin allocator shell

---
+++ src/RoundRobinAllocator.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.25;
+
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Ownable2StepUpgradeable} from "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
+
+contract RoundRobinAllocator is
+    UUPSUpgradeable,
+    Ownable2StepUpgradeable
+{
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal override onlyOwner {}
+
+    function initialize(address initialOwner) public initializer {
+        __Ownable2Step_init();
+        __Ownable_init(initialOwner);
+        __UUPSUpgradeable_init();
+    }
+}
