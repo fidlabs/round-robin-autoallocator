de5c1cfa38db71a2d48c47d168604168e39cb52f
lukas <lukasz.wal@neti-soft.com>
2025-04-08 14:40:25 +0200

    Show only info about test coverage without error when is not 100%

--- ci/check-full-coverage.sh
+++ ci/check-full-coverage.sh
@@ -14,12 +14,14 @@ summary=$(lcov --summary lcov.info --rc lcov_branch_coverage=1)
 lines_coverage=$(echo "$summary" | awk '/lines/{print $2}')
 functions_coverage=$(echo "$summary" | awk '/functions/{print $2}')
 branches_coverage=$(echo "$summary" | awk '/branches/{print $2}')
+
 echo "Lines coverage: $lines_coverage"
 echo "Functions coverage: $functions_coverage"
 echo "Branches coverage: $branches_coverage"
-if [ "$lines_coverage" == "100.0%" ] && [ "$functions_coverage" == "100.0%" ] && [ "$branches_coverage" == "100.0%" ]; then
-    echo "Coverage is 100% for lines, functions and branches."
-else
-    echo "Coverage is not 100%."
-    exit 1
-fi
+
+# if [ "$lines_coverage" == "100.0%" ] && [ "$functions_coverage" == "100.0%" ] && [ "$branches_coverage" == "100.0%" ]; then
+#     echo "Coverage is 100% for lines, functions and branches."
+# else
+#     echo "Coverage is not 100%."
+#     exit 1
+# fi
